eb66adcf408e02887be694a1d1dd15176247ab93
Richard Russon <rich@flatcap.org>
2018-05-10 16:39:25 +0100

    update doxygen support files

--- doxygen/gen-tables.sh
+++ doxygen/gen-tables.sh
@@ -16,11 +16,17 @@ function gen_config()
 	echo "/**"
 	grep " @page " "$FILE"
 	echo " *"
-	echo " * | Config Item | Global Variable | Description"
-	echo " * | :---------- | :-------------- | :----------"
+	echo " * | Config Item | Global Variable | Description |"
+	echo " * | :---------- | :-------------- | :---------- |"
 
 	for L in ${LINES[*]}; do
-		if [[ "$L" =~ .*[[:space:]*](.*)\;[[:space:]]*/\*\*\<[[:space:]]Config:[[:space:]](.*)[[:space:]]\*/ ]]; then
+		if [[ "$L" =~ .*[[:space:]*](.*)[[:space:]]=[[:space:]].*\;[[:space:]]*/\*\*\<[[:space:]]Config:[[:space:]](.*)[[:space:]]\*/ ]]; then
+			VAR="${BASH_REMATCH[1]}"
+			DESC="${BASH_REMATCH[2]}"
+			CFG="$(echo "$VAR" | sed -e 's/[A-Z]/_\l&/g' -e 's/^_//')"
+
+			echo " * | '$CFG' | #$VAR | $DESC |"
+		elif [[ "$L" =~ .*[[:space:]*](.*)\;[[:space:]]*/\*\*\<[[:space:]]Config:[[:space:]](.*)[[:space:]]\*/ ]]; then
 			VAR="${BASH_REMATCH[1]}"
 			DESC="${BASH_REMATCH[2]}"
 			CFG="$(echo "$VAR" | sed -e 's/[A-Z]/_\l&/g' -e 's/^_//')"
@@ -30,6 +36,7 @@ function gen_config()
 	done
 
 	echo " */"
+	echo
 }
 
 function gen_data()
@@ -38,22 +45,26 @@ function gen_data()
 	local LINES=()
 
 	LINES+=($(grep "^ \\* [A-Z][A-Za-z0-9_]\\+ - " "$FILE"))
-	LINES+=($(grep "/\\*\\*< " "$FILE" | grep -v "Config:"))
+	LINES+=($(grep "^[^ ].*/\\*\\*< " "$FILE" | grep -v "Config:"))
 
 	[ ${#LINES[@]} = 0 ] && return
 
 	echo "/**"
 	grep " @page " "$FILE"
 	echo " * "
-	echo " * | Data | Description"
-	echo " * | :--- | :----------"
+	echo " * | Data | Description |"
+	echo " * | :--- | :---------- |"
 
 	(
 	for L in ${LINES[*]}; do
 		if [[ "$L" =~ [[:space:]*]*(.*)[[:space:]]-[[:space:]](.*) ]]; then
 			VAR="${BASH_REMATCH[1]}"
 			DESC="${BASH_REMATCH[2]}"
-			# echo " * | #$VAR | $DESC |"
+			echo " * | #$VAR | $DESC |"
+		elif [[ "$L" =~ .*[[:space:]*]+([A-Za-z0-9_]+)[[:space:]]=[[:space:]].*\;[[:space:]]*/\*\*\<[[:space:]](.*)[[:space:]]\*/ ]]; then
+			VAR="${BASH_REMATCH[1]}"
+			DESC="${BASH_REMATCH[2]}"
+			echo " * | #$VAR | $DESC |"
 		elif [[ "$L" =~ .*[[:space:]*]+([A-Za-z0-9_]+)\;[[:space:]]*/\*\*\<[[:space:]](.*)[[:space:]]\*/ ]]; then
 			VAR="${BASH_REMATCH[1]}"
 			DESC="${BASH_REMATCH[2]}"
@@ -63,6 +74,7 @@ function gen_data()
 	) | sort
 
 	echo " */"
+	echo
 }
 
 function gen_functions()
@@ -77,19 +89,23 @@ function gen_functions()
 	echo "/**"
 	grep " @page " "$FILE"
 	echo " * "
-	echo " * | Function | Description"
-	echo " * | :------- | :----------"
+	echo " * | Function | Description |"
+	echo " * | :------- | :---------- |"
 
 	for L in ${LINES[*]}; do
-		if [[ "$L" =~ [[:space:]*]*((mutt|default)_.*)[[:space:]]-[[:space:]](.*) ]]; then
+		if [[ "$L" =~ ^[[:space:]*]*((crypto*_|driver_|getdns|imap_|log_|mutt_|mx_|nntp_|pgp_|pop_|raw_|rfc1524_|smime_).*)[[:space:]]-[[:space:]](.*) ]]; then
 			FUNC="${BASH_REMATCH[1]}"
 			DESC="${BASH_REMATCH[3]}"
-
+			echo " * | $FUNC() | $DESC |"
+		elif [[ "$L" =~ ^[[:space:]*]*(edit_or_view_message|feature_enabled|is_from|log_disp_curses|main|menu_status_line|print_copyright|print_version|safe_asprintf)[[:space:]]-[[:space:]](.*) ]]; then
+			FUNC="${BASH_REMATCH[1]}"
+			DESC="${BASH_REMATCH[2]}"
 			echo " * | $FUNC() | $DESC |"
 		fi
 	done
 
 	echo " */"
+	echo
 }
 
 
