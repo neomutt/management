86bf61541a117a9fc2c853e44fea9d01fd71229f
Richard Russon <rich@flatcap.org>
2018-05-12 17:14:41 +0100

    update templates

--- release-templates/website.vim
+++
@@ -1,3 +0,0 @@
-" linkify (@nick-name)
-
-%s!(@\(.\{-}\))!([@\1](https://github.com/\1))!
--- update-distro-bin/copr.sh
+++ update-distro-bin/copr.sh
@@ -41,7 +41,7 @@ echo
 pushd copr
 
 SPEC="neomutt.spec"
-OS="fc27"
+OS="fc28"
 HERE=$(pwd)
 
 echo "Edit: $SPEC"
