be20c67045c553ea5db6bf1b37989b6d01ca4568
Richard Russon <rich@flatcap.org>
2018-06-17 00:41:21 +0100

    release installer

---
+++ travis/release-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit # set -e
+set -o nounset # set -u
+
+SOURCE="${0%/*}"
+
+YAML="$SOURCE/release.yml"
+RELDIR="$SOURCE/release"
+TRAVIS=".travis.yml"
+
+git checkout -b release2 master
+
+mkdir .travis
+cp -v "$YAML" "$TRAVIS"
+cp -v "$RELDIR/gitignore" .gitignore
+cp -v "$RELDIR"/*.txt     .travis
+cp -v "$RELDIR"/*.rc      .travis
+cp -v "$RELDIR"/*.sh      .travis
+cp -v "$RELDIR/README.md" .
+
+git add "$TRAVIS" .travis .gitignore README.md
+git commit -m "travis: add release config"
+git log --oneline -n1
+
