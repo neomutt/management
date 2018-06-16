cdfb1338ad1cc4ea5557ac460a536a30c8a6784e
Richard Russon <rich@flatcap.org>
2018-06-17 00:58:14 +0100

    add missing translate files

--- travis/translate-install.sh
+++ travis/translate-install.sh
@@ -6,12 +6,17 @@ set -o nounset # set -u
 SOURCE="${0%/*}"
 
 YAML="$SOURCE/translate.yml"
+TRANSDIR="$SOURCE/translate"
 TRAVIS=".travis.yml"
 
 git checkout -b translate master
 
+mkdir .travis
 cp -v "$YAML" "$TRAVIS"
-git add "$TRAVIS"
+cp -v "$TRANSDIR"/*.sh  .travis
+cp -v "$TRANSDIR"/*.enc .travis
+
+git add "$TRAVIS" .travis
 git commit -m "travis: add translate config" -m "[ci skip]"
 git log --oneline -n1
 
