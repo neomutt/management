81dd08e345260a98c2bb0d559948f391031d7f35
Richard Russon <rich@flatcap.org>
2019-03-09 15:53:54 +0000

    update iwyu scripts

--- iwyu/i-main.sh
+++ iwyu/i-main.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-iwyu \
-	-D_ALL_SOURCE=1 \
-	-D_GNU_SOURCE=1 \
-	-D__EXTENSIONS__ \
-	-DNCURSES_WIDECHAR \
-	-DDEBUG \
-	-I . \
-	-I /usr/lib/gcc/x86_64-redhat-linux/8/include \
-	-I /usr/include/qdbm \
-	-Xiwyu --mapping_file=neomutt.imp \
-	-Xiwyu --no_comments \
-	-Xiwyu --pch_in_code \
-	"$@"
+BASE_DIR="${0%/*}"
 
-	# -DPKGDATADIR="/usr/share/neomutt" \
-	# -DSYSCONFDIR="/etc" \
-	# -DSIG_ATOMIC_VOLATILE_T=volatile \
-	# -DLOFF_T=long \
+for i in "$@"; do
+	iwyu \
+		-D_ALL_SOURCE=1 \
+		-D_GNU_SOURCE=1 \
+		-D__EXTENSIONS__ \
+		-DNCURSES_WIDECHAR \
+		-DDEBUG \
+		-I . \
+		-I /usr/lib/gcc/x86_64-redhat-linux/8/include \
+		-I /usr/include/qdbm \
+		-Xiwyu --mapping_file="$BASE_DIR/neomutt.imp" \
+		-Xiwyu --no_comments \
+		-Xiwyu --pch_in_code \
+		"$i"
+done
+
+		# -DPKGDATADIR="/usr/share/neomutt" \
+		# -DSYSCONFDIR="/etc" \
+		# -DSIG_ATOMIC_VOLATILE_T=volatile \
+		# -DLOFF_T=long \
--- iwyu/i-mutt.sh
+++ iwyu/i-mutt.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-iwyu \
-	-D_ALL_SOURCE=1 \
-	-D_GNU_SOURCE=1 \
-	-D__EXTENSIONS__ \
-	-DNCURSES_WIDECHAR \
-	-DDEBUG \
-	-DLOFF_T=long \
-	-DSIG_ATOMIC_VOLATILE_T=volatile \
-	-I . \
-	-I /usr/lib/gcc/x86_64-redhat-linux/8/include \
-	-Xiwyu --pch_in_code \
-	-Xiwyu --no_comments \
-	-Xiwyu --mapping_file=mutt.imp \
-	"$@"
+BASE_DIR="${0%/*}"
 
+for i in "$@"; do
+	iwyu \
+		-D_ALL_SOURCE=1 \
+		-D_GNU_SOURCE=1 \
+		-D__EXTENSIONS__ \
+		-DNCURSES_WIDECHAR \
+		-DDEBUG \
+		-I . \
+		-I mutt \
+		-I /usr/lib/gcc/x86_64-redhat-linux/8/include \
+		-Xiwyu --pch_in_code \
+		-Xiwyu --no_comments \
+		-Xiwyu --mapping_file="$BASE_DIR/mutt.imp" \
+		"$i"
+done
+
+		# -DLOFF_T=long \
+		# -DSIG_ATOMIC_VOLATILE_T=volatile \
