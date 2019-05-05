925219e8acce3b31abb4ae91ef4d7320dd65456a
Richard Russon <rich@flatcap.org>
2019-05-05 18:49:51 +0100

    add coveralls config

---
+++ travis/coveralls-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit # set -e
+set -o nounset # set -u
+
+SOURCE="${0%/*}"
+
+YAML="$SOURCE/coveralls.yml"
+TRAVIS=".travis.yml"
+
+git checkout -b coveralls master
+
+cp -v "$YAML" "$TRAVIS"
+git add "$TRAVIS"
+git commit -m "travis: add coveralls config"
+git log --oneline -n1
+
