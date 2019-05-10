fe80b86c0ce32e7212915fd4c836555323626a29
Joseph Bisch <joseph.bisch@gmail.com>
2019-05-10 15:07:20 +0100

    add fuzzer build

---
+++ travis/fuzzer-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit # set -e
+set -o nounset # set -u
+
+SOURCE="${0%/*}"
+
+RELDIR="$SOURCE/fuzzer"
+
+git checkout -b fuzzer master
+
+cp -r "$RELDIR"/fuzz .
+
+git apply "$RELDIR"/fuzzer.diff
+
+git add .gitignore Makefile.autosetup auto.def main.c fuzz
+git commit -m "travis: add fuzzer config"
+git log --oneline -n1
+
---
+++ travis/fuzzer/fuzz/address.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "options.h"
+#include <signal.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+#include "email/envelope.h"
+#include "email/body.h"
+#include "email/parse.h"
+#include "email/mime.h"
+#include "email/email.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+	OptNoCurses = 1;
+	char file[] = "/tmp/mutt-fuzz";
+	FILE *fp = fopen(file, "wb");
+	if (fp != NULL) {
+		fwrite(data, 1, size, fp);
+		fclose(fp);
+	}
+	fp = fopen(file, "rb");
+	struct Email *e = mutt_email_new();
+	struct Envelope *env = mutt_rfc822_read_header(fp, e, 0, 0);
+	mutt_parse_part(fp, e->content);
+	mutt_email_free(&e);
+	mutt_env_free(&env);
+	fclose(fp);
+	return 0;
+}
