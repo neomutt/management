bf2d3fca2330281781e00d189fa19a8f362204f3
Zero King <l2dy@icloud.com>
2019-10-22 18:57:46 +0100

    fix macports build

--- travis/macports/bootstrap.sh
+++ travis/macports/bootstrap.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
-curl -fsSLO "https://raw.githubusercontent.com/Homebrew/install/master/uninstall"
-chmod 0755 uninstall && ./uninstall -fq && rm -f uninstall
+
+# Uninstall Homebrew
+brew --version
 /usr/bin/sudo /usr/bin/find /usr/local -mindepth 2 -delete && hash -r
-export OS_MAJOR=$(uname -r | cut -f 1 -d .)
-curl -fsSLO "https://dl.bintray.com/macports-ci-bot/macports-base/MacPorts-${OS_MAJOR}.tar.bz2"
+
+# Download and install MacPorts built by https://github.com/macports/macports-base/blob/travis-ci/.travis.yml
+OS_MAJOR=$(uname -r | cut -f 1 -d .)
+curl -fsSLO "https://dl.bintray.com/macports-ci-bot/macports-base/2.6r0/MacPorts-${OS_MAJOR}.tar.bz2"
 sudo tar -xpf "MacPorts-${OS_MAJOR}.tar.bz2" -C /
 rm -f "MacPorts-${OS_MAJOR}.tar.bz2"
+
 unset CC && source /opt/local/share/macports/setupenv.bash
 echo "ui_interactive no" | sudo tee -a /opt/local/etc/macports/macports.conf
 sudo /opt/local/postflight && sudo rm -f /opt/local/postflight
 sudo port -v sync
-set +e
