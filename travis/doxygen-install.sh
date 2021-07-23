e8775eb63b7bdfbccff96329223f1841ba713765
Richard Russon <rich@flatcap.org>
2021-07-23 22:18:09 +0100

    doxy: sync to code

--- travis/doxygen-install.sh
+++ travis/doxygen-install.sh
@@ -13,13 +13,12 @@ git checkout -b doxygen master
 
 cp -v "$YAML" "$TRAVIS"
 cp -v "$DOXYDIR/gitignore" .gitignore
-cp -v "$DOXYDIR"/*.inc .
 cp -v "$DOXYDIR/doxygen/"* doxygen
 
 git apply "$DOXYDIR/doxygen-extras.patch"
 
 git rm .cirrus.yml
-git add -f "$TRAVIS" doxygen *.inc mutt/lib.h email/lib.h Makefile.autosetup .gitignore
+git add -f "$TRAVIS" doxygen mutt/lib.h email/lib.h Makefile.autosetup .gitignore
 git commit -m "travis: add doxygen config"
 git log --oneline -n1
 
