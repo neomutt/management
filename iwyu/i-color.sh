88fd95315f6bb3ff6fb9a6fd11930d0fe3c0ded8
Richard Russon <rich@flatcap.org>
2021-08-18 15:48:24 +0100

    iwyu libcolor

---
+++ iwyu/i-color.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASE_DIR="${0%/*}"
+
+for i in "$@"; do
+	[ "$i" = "mutt_curses.h" ] && continue
+
+	iwyu \
+		-D_ALL_SOURCE=1 \
+		-D_GNU_SOURCE=1 \
+		-D__EXTENSIONS__ \
+		-DNCURSES_WIDECHAR \
+		-DDEBUG \
+		-I . \
+		-I color \
+		-I /usr/lib/gcc/x86_64-redhat-linux/11/include \
+		-I /usr/include/qdbm \
+		-Xiwyu --mapping_file="$BASE_DIR/color.imp" \
+		-Xiwyu --no_comments \
+		-Xiwyu --pch_in_code \
+		"$i"
+done
+
