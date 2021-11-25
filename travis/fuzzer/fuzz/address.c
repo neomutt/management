2a5e04db10e6c800c064a7d52745cf7d3294a579
Richard Russon <rich@flatcap.org>
2021-11-25 17:27:21 +0000

    fuzzer: create dummy config

--- travis/fuzzer/fuzz/address.c
+++ travis/fuzzer/fuzz/address.c
@@ -1,31 +1,37 @@
-#include <stdlib.h>
-#include <stdint.h>
-
-#include "options.h"
 #include <signal.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <sys/stat.h>
-
-#include "email/envelope.h"
+#include "config/set.h"
 #include "email/body.h"
-#include "email/parse.h"
-#include "email/mime.h"
 #include "email/email.h"
+#include "email/envelope.h"
+#include "email/mime.h"
+#include "email/parse.h"
+#include "core/neomutt.h"
+#include "options.h"
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-	OptNoCurses = 1;
-	char file[] = "/tmp/mutt-fuzz";
-	FILE *fp = fopen(file, "wb");
-	if (fp != NULL) {
-		fwrite(data, 1, size, fp);
-		fclose(fp);
-	}
-	fp = fopen(file, "rb");
-	struct Email *e = email_new();
-	struct Envelope *env = mutt_rfc822_read_header(fp, e, 0, 0);
-	mutt_parse_part(fp, e->body);
-	email_free(&e);
-	mutt_env_free(&env);
-	fclose(fp);
-	return 0;
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+  struct ConfigSet *cs = NULL;
+  NeoMutt = neomutt_new(cs);
+  OptNoCurses = 1;
+  char file[] = "/tmp/mutt-fuzz";
+  FILE *fp = fopen(file, "wb");
+  if (fp != NULL)
+  {
+    fwrite(data, 1, size, fp);
+    fclose(fp);
+  }
+  fp = fopen(file, "rb");
+  struct Email *e = email_new();
+  struct Envelope *env = mutt_rfc822_read_header(fp, e, 0, 0);
+  mutt_parse_part(fp, e->body);
+  email_free(&e);
+  mutt_env_free(&env);
+  fclose(fp);
+  neomutt_free(&NeoMutt);
+  cs_free(&cs);
+  return 0;
 }
