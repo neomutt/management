48ae96f57d1e0a50c1f2b97d9bff1d5444cf9d84
Richard Russon <rich@flatcap.org>
2022-04-28 00:11:44 +0100

    iwyu: libmixmaster

---
+++ iwyu/i-mixmaster.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BASE_DIR="${0%/*}"
+
+for i in "$@"; do
+	iwyu \
+		-D_ALL_SOURCE=1 \
+		-D_GNU_SOURCE=1 \
+		-D__EXTENSIONS__ \
+		-DNCURSES_WIDECHAR \
+		-DDEBUG \
+		-I . \
+		-I mixmaster \
+		-I /usr/lib/gcc/x86_64-redhat-linux/11/include \
+		-Xiwyu --pch_in_code \
+		-Xiwyu --no_comments \
+		-Xiwyu --mapping_file="$BASE_DIR/mixmaster.imp" \
+		"$i"
+done
+
