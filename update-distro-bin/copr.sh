65d60b004d8761194a78847b58ffed2877d49b74
Richard Russon <rich@flatcap.org>
2022-07-09 13:01:32 +0100

    update copr script

--- update-distro-bin/copr.sh
+++ update-distro-bin/copr.sh
@@ -40,19 +40,20 @@ rpmdev-setuptree
 
 cp ../${VERSION}.tar.gz rpmbuild/SOURCES/neomutt-${VERSION}.tar.gz
 cp *.patch              rpmbuild/SOURCES
+cp fedora-colors.rc     rpmbuild/SOURCES
 
 rpmbuild -bs --target=noarch --define=_topdir\ $HERE/rpmbuild "$SPEC"
 
 cp rpmbuild/SRPMS/neomutt-${VERSION}-1.${OS}.src.rpm .
 
 rm -fr rpmbuild
-exit 0
+# exit 0
 rpmdev-setuptree
 
 rpmbuild --rebuild --define=_topdir\ $HERE/rpmbuild neomutt-${VERSION}-1.${OS}.src.rpm
 
 cp rpmbuild/RPMS/*/*.rpm .
 
-# rm -fr rpmbuild
+rm -fr rpmbuild
 popd
 
